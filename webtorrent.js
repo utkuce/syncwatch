da9768f3b850c00c8713db97037ca20e8813ba15
utku <utkuce@gmail.com>
2019-08-24 04:44:19 -0400

    Add more torrent info

--- webtorrent.js
+++ webtorrent.js
@@ -23,12 +23,20 @@ function setTorrentSource(torrentId) {
         }); */
     
         var interval = setInterval(function () {
-            torrentProgress.innerHTML = "Downloading: " + (torrent.progress * 100).toFixed(1) + "%";
+            torrentProgress.innerHTML = "Downloading: " + (torrent.progress * 100).toFixed(1) + "%" 
+                + " - " + (torrent.downloadSpeed / Math.pow(10,6)).toFixed(2)  + " mb/s <i class='fas fa-long-arrow-alt-down'></i>"
+                + " from " + torrent.numPeers + " peer(s)"
+                + " Time left: " + (torrent.timeRemaining/60000).toFixed(2) + " minutes";
+
         }, 1000);
     
         torrent.on("done", function () {
             torrentProgress.innerHTML = "File ready. (" + file.name + ")";
             clearInterval(interval);
+        });
+
+        torrent.on('error', function (err) {
+            alert(err);
         });
     })
 }
@@ -41,7 +49,7 @@ function handleFileSelect (evt) {
   var files = evt.target.files // FileList object
   var videoFile = files[0];
 
-  client.seed(videoFile, function (torrent) {
+  client.seed(videoFile, {announceList: [["wss://tracker.openwebtorrent.com "]]}, function (torrent) {
         
         console.log('Client is seeding ' + torrent.magnetURI);
 
@@ -50,5 +58,17 @@ function handleFileSelect (evt) {
         var newSource = JSON.stringify({ "newSource": { "url": torrent.magnetURI} });
         log("Sending data: " + newSource)
         ws.send(newSource);
+
+        var interval = setInterval(function () {
+            torrentProgress.innerHTML = "Seeding: " 
+                + (torrent.uploadSpeed / Math.pow(10,6)).toFixed(2)  + " mb/s <i class='fas fa-long-arrow-alt-up'></i>"
+                + " for " + torrent.numPeers + " peer(s)";
+
+        }, 1000);
+
+        torrent.on('error', function (err) {
+            alert(err);
+        });
+
     });
 }
